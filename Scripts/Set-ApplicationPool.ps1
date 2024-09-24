673d1eea2e7b05f37a6405204422d15b925039b2
Sam Howard <s.howard@elbglobal.com>
2024-09-24 13:54:46 -0700

    authtype adding quotes

--- Scripts/Set-ApplicationPool.ps1
+++ Scripts/Set-ApplicationPool.ps1
@@ -15,7 +15,7 @@ $msdeployArguments = @(
     "-verb:sync",
     "-allowUntrusted",
     "-source:recycleApp",
-    "-dest:recycleApp=${recycleApp},recycleMode=${recycleMode},computerName=${computerNameArgument},username=${username},password=${password},AuthType=Basic"  # Removed quotes around Basic
+    "-dest:recycleApp=${recycleApp},recycleMode=${recycleMode},computerName=${computerNameArgument},username=${username},password=${password},AuthType='Basic'"
 )
 
 # Execute msdeploy with the arguments
