03e8d2c7b3f1bc3a731bf5771763d047af3cf8e2
Sam Howard <s.howard@elbglobal.com>
2024-09-24 11:53:19 -0700

    modify msdeployArguments

--- Scripts/Set-ApplicationPool.ps1
+++ Scripts/Set-ApplicationPool.ps1
@@ -1,25 +1,22 @@
+$msdeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe"
 
-$msdeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe";
-
+# Read arguments
 $recycleMode = $args[0]
 $recycleApp = $args[1]
 $computerName = $args[2]
 $username = $args[3]
 $password = $args[4]
 
-$computerNameArgument = $computerName + '/MsDeploy.axd?site=' + $recycleApp
+# Create the computer name argument
+$computerNameArgument = "${computerName}/MsDeploy.axd?site=${recycleApp}"
 
-$msdeployArguments = 
+# Construct msdeploy arguments
+$msdeployArguments = @(
     "-verb:sync",
     "-allowUntrusted",
     "-source:recycleApp",
-    ("-dest:" + 
-        "recycleApp=${recycleApp}," +
-        "recycleMode=${recycleMode}," +
-        "computerName=${computerNameArgument}," + 
-        "username=${username}," +
-        "password=${password}," +
-        "AuthType='Basic'"
-    )
+    "-dest:recycleApp=${recycleApp},recycleMode=${recycleMode},computerName=${computerNameArgument},username=${username},password=${password},AuthType='Basic'"
+)
 
-& $msdeploy $msdeployArguments
+# Execute msdeploy with the arguments
+& $msdeploy @msdeployArguments
